5ad1f9d90ef36a43023c0fee9e899a4369cbf110
Miroslav Petrov <miroslav.petrov@modacom.com>
2015-07-15 16:38:48 +0300

    add comment

--- app/code/local/Metrilo/Analytics/Helper/Data.php
+++ app/code/local/Metrilo/Analytics/Helper/Data.php
@@ -37,6 +37,13 @@ class Metrilo_Analytics_Helper_Data extends Mage_Core_Helper_Abstract
         return Mage::getStoreConfig('metrilo_analytics_settings/settings/api_secret');
     }
 
+    /**
+     * Add event to queue
+     * 
+     * @param string $method Can be identiy|track
+     * @param string $type
+     * @param string|array $data
+     */
     public function addEvent($method, $type, $data)
     {
         $events = (array)$this->getSession()->getData(Metrilo_Analytics_Block_Head::DATA_TAG);
